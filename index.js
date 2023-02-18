3a2bb11a441f3aec176de30a4dedc55c4c551292
Quentin <quentingrd0706@gmail.com>
2023-02-18 20:21:00 +0100

    eslinter

---
+++ index.js
@@ -0,0 +1,12 @@
+const BIG_CONST = 432432;
+const myArray = [1,45,6]
+
+for (let index = 0; index < myArray.length; index++){
+    const element = myArray[index];
+    console.log(element)
+    console.log(element, 2, 1);
+}
+
+const myFunction = (a, b, c) => {
+    return a * b
+}
